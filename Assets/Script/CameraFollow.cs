fb4c5d9d56a7229036e28b20950f2bc0c495b393
Rahul Yadav <devmethyl@outlook.com>
2021-08-22 19:44:52 +0530

    Added Ship Controls & Hacky Camera Follow Ship

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform myTarget;
+    float maxSpeed = 3.1f;
+
+    private void Start()
+    {
+        PlayerMovement playerMovement = gameObject.GetComponent<PlayerMovement>();
+       // Vector3 pos = playerMovement.transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (pos.y + shipBoundaryRadius > Camera.main.orthographicSize)
+        //{
+        //    pos.y = Camera.main.orthographicSize - shipBoundaryRadius;
+        //    Debug.Log(pos);
+        //}
+
+        //if (myTarget != null)
+        //{
+        //    Vector3 targPos = myTarget.position;
+        //    targPos.z = transform.position.z;
+        //    transform.position = targPos;
+        //}
+
+        Vector3 pos = transform.position;
+        pos.y += maxSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float maxSpeed = 3f;
+    float sideMove = 3f;
+    float shipBoundaryRadius = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Roate the Ship
+        //Quaternion rot = transform.r
+        
+        //Move the Ship
+        Vector3 pos = transform.position;
+        // pos.y += Input.GetAxis("Vertical");
+        pos.y +=  maxSpeed * Time.deltaTime;
+        pos.x += Input.GetAxis("Horizontal") * sideMove * Time.deltaTime;
+       // pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            pos.y += Input.GetAxis("Vertical") * (-1) * Time.deltaTime;
+        }
+        transform.position = pos;
+
+        //Restrict Player to camera boundry
+        if (pos.y + shipBoundaryRadius > Camera.main.orthographicSize)
+        {
+            pos.y = Camera.main.orthographicSize - shipBoundaryRadius;
+            Debug.Log(pos);
+        }
+
+        
+        
+    }
+}
